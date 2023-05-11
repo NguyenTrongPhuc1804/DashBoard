8c2886940813d88828dc52ded6f341fc76ae4973
Nguyen Trong Phuc <nguyentrongphuc1804@gmail.com>
2023-05-11 18:48:08 +0700

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from "vue";
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <demo-form />
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
